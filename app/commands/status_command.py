5e56c5e64f2930b056bd156fdaa0ae4062df7ec3
Hetorx <arkadiuszkoziol2@gmail.com>
2022-12-02 16:15:07 +0100

    commit

--- app/commands/status_command.py
+++ app/commands/status_command.py
@@ -1,17 +1,22 @@
+from app import test
 import time
 import requests
 import psutil
 from app.utility import second_parser
+from app.utility import Database_connection_check
+@test.client.command()
+async def status(ctx):
 
-# Latency measurement
-url = "https://discord.com"
-get_uptime = requests.get(url).elapsed.total_seconds()
+    # Latency measurement
+    url = "https://discord.com"
+    get_uptime = requests.get(url).elapsed.total_seconds()
 
-# Uptime since running pc
-def uptime():
-    return time.time() - psutil.boot_time()
+    # Uptime since running pc
+    uptime = time.time() - psutil.boot_time()
 
-
-print("Uptime:", second_parser.parse_duration(uptime()))
-print("Latency is:", int(get_uptime * 1000), "ms")
-print("Available Memory:", psutil.virtual_memory()[1] // 1000000, "MB")
+    await ctx.send\
+        (f"Uptime: {second_parser.parse_duration(uptime)}\n"
+        f"Latency is: {int(get_uptime * 1000)}ms \n"
+        f"Available Memory: {psutil.virtual_memory()[1] // 1000000}MB\n"
+        f"Database connection: {Database_connection_check}"
+         )
